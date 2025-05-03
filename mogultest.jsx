7c86916d61429a60fa8ec4f72ae5246118d27bd5
jasoncurescu <106756134+jasoncurescu@users.noreply.github.com>
2025-05-03 14:17:55 -0500

    Create mogultest.jsx

---
+++ mogultest.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import R2R010131 from "./13-00455r2-r01-013-1.png";
+import "./mogulteststyle.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="text-wrapper">Currently in Chicago, IL</div>
+
+        <div className="text-wrapper-2">Sometimes I make pizza</div>
+
+        <div className="text-wrapper-3">LinkedIn</div>
+
+        <div className="text-wrapper-4">Instagram</div>
+
+        <div className="text-wrapper-5">Flickr</div>
+
+        <div className="text-wrapper-6">Tumblr</div>
+
+        <div className="text-wrapper-7">jasoncurescu@gmail.com</div>
+
+        <div className="text-wrapper-8">2020 - 2023</div>
+
+        <div className="text-wrapper-9">2018 - NOW</div>
+
+        <div className="text-wrapper-10">2016 - 2018</div>
+
+        <div className="text-wrapper-11">2015 - 2016</div>
+
+        <p className="senior-product">
+          <span className="span">Senior product designer at Mogul</span>
+        </p>
+
+        <div className="text-wrapper-12">View projects</div>
+
+        <div className="text-wrapper-13">View project</div>
+
+        <div className="text-wrapper-14">View project</div>
+
+        <div className="text-wrapper-15">View project</div>
+
+        <p className="co-founded-aina">
+          <span className="span">Co-founded Aina</span>
+        </p>
+
+        <p className="product-designer-at">
+          <span className="span">Product designer at Otus</span>
+        </p>
+
+        <div className="text-wrapper-16">Graphic designer at Carnegie</div>
+
+        <div className="rectangle" />
+
+        <div className="rectangle-2" />
+
+        <div className="rectangle-3" />
+
+        <div className="rectangle-4" />
+
+        <div className="rectangle-5" />
+
+        <img className="element" alt="Element" src={R2R010131} />
+
+        <p className="p">
+          I’m a senior product designer creating the worlds best recruitment
+          software at Mogul.
+        </p>
+
+        <div className="text-wrapper-17">Jason Curescu</div>
+      </div>
+    </div>
+  );
+};
