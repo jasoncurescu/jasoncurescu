ef1b66cc72eb6be98db72c7f4f95b3d349dfb347
jasoncurescu <106756134+jasoncurescu@users.noreply.github.com>
2025-05-03 20:41:23 -0500

    Delete mogulscript.js

--- mogulscript.js
+++
@@ -1,7 +0,0 @@
-// Example: Toggle navigation menu on mobile
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
